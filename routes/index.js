c528d99c5e71c265745ac4321f75feb082f06830
Mehmet Seven <mehmetseven0@gmail.com>
2018-08-28 17:44:58 +0300

    homepage redirect

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+	if (!req.user)
+		res.render('index', { title: 'Express' });
+	else
+		res.redirect('/chat');
 });
 
 module.exports = router;
