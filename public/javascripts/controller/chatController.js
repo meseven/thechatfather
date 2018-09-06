f05ac1d3dded7a3f3d227b17224965624a937d37
Mehmet Seven <mehmetseven0@gmail.com>
2018-09-06 14:00:46 +0300

    socker.io-redis integration

--- public/javascripts/controller/chatController.js
+++ public/javascripts/controller/chatController.js
@@ -1,3 +1,7 @@
 app.controller('chatController', ['$scope', ($scope) => {
 	const socket = io.connect("http://localhost:3000");
+
+	socket.on('hello', () => {
+		console.log('hello');
+	});
 }]);
--- src/socketApi.js
+++ src/socketApi.js
@@ -6,8 +6,19 @@ const socketApi = {
 	io
 };
 
+/**
+ * Redis adapter
+ */
+const redisAdapter = require('socket.io-redis');
+io.adapter(redisAdapter({
+	host: process.env.REDIS_URI,
+	port: process.env.REDIS_PORT
+}));
+
 io.on('connection', socket => {
 	console.log('a user logged in');
+
+	socket.broadcast.emit('hello');
 });
 
 
