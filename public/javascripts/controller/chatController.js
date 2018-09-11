9237fbc05d4801981e801f3ead0864ec7a73c0a6
Mehmet Seven <mehmetseven0@gmail.com>
2018-09-11 16:04:52 +0300

    client messages

--- public/javascripts/controller/chatController.js
+++ public/javascripts/controller/chatController.js
@@ -41,11 +41,21 @@ app.controller('chatController', ['$scope', 'userFactory','chatFactory', ($scope
 	});
 
 	$scope.newMessage = () => {
-		socket.emit('newMessage', {
-			message: $scope.message,
-			roomId: $scope.roomId
-		});
-		$scope.message = '';
+		if ($scope.message.trim() !== '') {
+			socket.emit('newMessage', {
+				message: $scope.message,
+				roomId: $scope.roomId
+			});
+
+			$scope.messages[$scope.roomId].push({
+				userId: $scope.user._id,
+				username: $scope.user.name,
+				surname: $scope.user.surname,
+				message: $scope.message
+			});
+
+			$scope.message = '';
+		}
 	};
 
 	$scope.switchRoom = room => {
